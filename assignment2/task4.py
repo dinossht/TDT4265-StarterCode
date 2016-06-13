163c1fbffcf2c9a550e85f57bf1f91ec83bd507e
fwe <were>
2016-06-13 03:05:19 +0200

    fix labels and plots for task 4a

--- assignment2/task4.py
+++ assignment2/task4.py
@@ -136,10 +136,10 @@ if __name__ == "__main__":
         print("Run:", run)
         
         # Settings for task 3. Keep all to false for task 2.
-        use_shuffle                 = False
-        use_improved_sigmoid        = False
-        use_improved_weight_init    = False
-        use_momentum                = False
+        use_shuffle                 = True
+        use_improved_sigmoid        = True
+        use_improved_weight_init    = True
+        use_momentum                = True
 
         if run == 1:
             # task 3a)    
@@ -206,7 +206,7 @@ if __name__ == "__main__":
 
     plt.xlabel("Number of gradient steps")
     plt.ylabel("Cross Entropy Loss")
-    plt.legend(["Training Loss", "Validation Loss", "Training Loss with momentum", "Validation Loss with momentum"])
+    plt.legend(["Training Loss with 64 hidden neurons", "Validation Loss with 64 hidden neurons", "Training Loss with 16 hidden neurons", "Validation Loss with 16 hidden neurons"])
     
     # Plot accuracy
     plt.subplot(1, 2, 2)
@@ -220,7 +220,7 @@ if __name__ == "__main__":
         loss = list(val_accuracy_all[run].values())
         plt.plot(global_steps, loss, fmt[run])
 
-    plt.legend(["Training Accuracy", "Validation Accuracy", "Training Accuracy with momentum", "Validation Accuracy with momentum"])
+    plt.legend(["Training Accuracy with 64 hidden neurons", "Validation Accuracy with 64 hidden neurons", "Training Accuracy with 16 hidden neurons", "Validation Accuracy with 16 hidden neurons"])
     plt.xlabel("Number of gradient steps")
     plt.ylabel("Accuracy")
     
