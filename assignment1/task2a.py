d1076b71ab4465822a6bf9b9ae3db56957f26858
Dino School <dinossht@stud.ntnu.no>
2020-01-23 16:24:07 +0100

    Add L2 regularization

--- assignment1/task2a.py
+++ assignment1/task2a.py
@@ -72,7 +72,7 @@ class BinaryModel:
         # input: x
         # dim(grad) = dim(w) = 785
         N = targets.shape[0]
-        self.grad = -X.T@(targets-outputs) / N
+        self.grad = (-X.T@(targets-outputs)+self.l2_reg_lambda*2*self.w) / N
         assert self.grad.shape == self.w.shape,\
             f"Grad shape: {self.grad.shape}, w: {self.w.shape}"
         
