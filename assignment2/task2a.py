2774c1c56201ab5dcf82d6de72005d0db8a902ab
Dino School <dinossht@stud.ntnu.no>
2020-02-06 13:54:16 +0100

    add: training data normalization

--- assignment2/task2a.py
+++ assignment2/task2a.py
@@ -13,10 +13,13 @@ def pre_process_images(X: np.ndarray):
     """
     assert X.shape[1] == 784,\
         f"X.shape[1]: {X.shape[1]}, should be 784"
-
-    # Normalize from [0,255] to [0,1]
+    # Input normalization
+    # Normalize using mean and standard deviation 
+    mu      = np.mean(X)
+    sigma   = np.std(X)
+    X_norm  = (X - mu) / sigma
     # Append 1 at the end (bias trick)
-    return np.append(X / 255.0, np.ones((X.shape[0], 1)), axis=1)
+    return np.append(X_norm, np.ones((X.shape[0], 1)), axis=1)
 
 
 def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray):
